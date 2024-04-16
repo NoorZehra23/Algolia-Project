91c303c2692fd664174f0c12da0c5ebef62e4e01
noorzehra23 <syeda.noor@dubizzlelabs.com>
2024-04-16 18:43:04 +0500

    Final
    Used Algolia for Instant Search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import SearchPage from './SearchPage';
+import Details from './Details';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' >
+      <Route
+        path=''
+        element={<SearchPage />} />
+      <Route path='/details/:userId' element={<Details />} />
+    </Route>
+  )
+)
+
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/SearchPage.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch';
+import { useNavigate } from 'react-router-dom';
+
+const searchClient = algoliasearch('H883WZN5K5', 'c4c34621505ccdbb7ec2d7f8907571c4');
+
+function Hit({ hit }) {
+    const navigate = useNavigate()
+  const handleClick = () => {
+    const url = `/details/${hit.id}`;
+        navigate(url, { state: hit});
+  };
+
+  return (
+    <div className="flex flex-col h-full items-center justify-center rounded overflow-hidden shadow-lg hover:shadow-2xl transition duration-300" onClick={handleClick}>
+      <div className='w- h-64 mt-4 p-2'>
+        <img className="w-full h-full object-cover" src={hit.image} alt={hit.firstName} />
+      </div>
+      <div className="px-6 py-4 flex-grow">
+        <div className="font-bold text-xl mb-2">{hit.firstName} {hit.lastName} </div>
+      </div>
+    </div>
+  );
+}
+
+const SearchPage = () => (
+  <InstantSearch searchClient={searchClient} indexName='users_test'>
+    <SearchBox classNames={{
+      root: 'flex items-center justify-center align-center',
+      form: 'mx-auto max-w-md p-6 bg-white rounded-md shadow-md',
+      input: 'px-2 py-1 border border-gray-300 focus:outline-none',
+      submit: 'px-2 py-3 ml-2  text-white hover:bg-blue-600 focus:outline-none'
+    }}
+      placeholder='Search for Users'
+    />
+    <div className="flex flex-wrap justify-center">
+      <Hits hitComponent={Hit} classNames={{
+        list: 'flex flex-wrap justify-center items-center p-4 space-x-4',
+      }} />
+    </div>
+
+    <Pagination classNames={{
+      list: 'flex flex-wrap justify-center items-center p-4 space-x-4',
+      item: 'hover:text-blue-500'
+    }} />
+  </InstantSearch>
+);
+
+export default SearchPage;
