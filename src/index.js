5e4120071e3c49112b5286b6efc81634011464f4
noorzehra23 <syeda.noor@dubizzlelabs.com>
2024-04-16 18:38:23 +0500

    New Project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+// import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
